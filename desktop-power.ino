75859623fb640d1b3efdd8529aa37f483509e418
Jonathan <jonni@Jonathans-MacBook-Pro.fritz.box>
2016-07-08 10:29:08 +0200

    Added OTA

--- desktop-power.ino
+++ desktop-power.ino
@@ -1,7 +1,14 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
-// Wifi + Server Configuration
+//OTA Config
+const char* deviceName = "ESP8266-Desktop-Power";
+const char* OTAPass = "update";
+
+// Wifi network details and MQTT broker IP
 const char* ssid = "*****";
 const char* password = "*****";
 const char* mqtt_server = "*****";
@@ -82,9 +89,41 @@ void reconnect() {
 void setup()
 {
   Serial.begin(115200);
+
+  //OTA Setup
+  WiFi.mode(WIFI_STA);
+  ArduinoOTA.setPassword(OTAPass);
+  ArduinoOTA.setHostname(deviceName);
+  
   setup_wifi(); 
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  
   Serial.println("READY");
   
   pinMode(statusLED, OUTPUT);
@@ -102,4 +141,5 @@ void loop()
     reconnect();
   }
   client.loop();
+  ArduinoOTA.handle();
 }
